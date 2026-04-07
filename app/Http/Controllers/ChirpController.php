a541a78fa5344971f0a2705914fdde78764fd572
karnVen <onemonth06@gmail.com>
2026-04-07 13:39:43 +0000

    feat: model

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,42 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Chirp;
 use Illuminate\Http\Request;
 
 class ChirpController extends Controller
 {
     public function index()
     {
-        // For now, we manually create a list of data (The Menu)
-        $chirps = [
-            [
-                'author' => 'Karan',
-                'message' => 'I just built my first Controller! 🚀',
-                'time' => '1 minute ago'
-            ],
-            [
-                'author' => 'Gemini',
-                'message' => 'You are doing great, keep going!',
-                'time' => '10 minutes ago'
-            ],
-             [
-            'author' => 'Jane Doe',
-            'message' => 'Just deployed my first Laravel app! 🚀',
-            'time' => '5 minutes ago'
-        ],
-        [
-            'author' => 'John Smith',
-            'message' => 'Laravel makes web development fun again!',
-            'time' => '1 hour ago'
-        ],
-        [
-            'author' => 'Alice Johnson',
-            'message' => 'Working on something cool with Chirper...',
-            'time' => '3 hours ago'
-        ]
-        ];
+        $chirps = Chirp::with('user')
+            ->latest()
+            ->take(50)  // Limit to 50 most recent chirps
+            ->get();
 
-        // Send that data to the "home" view
         return view('home', ['chirps' => $chirps]);
     }
 }
