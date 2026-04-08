7eb02f51cb932b6629e1be39ff68aa3c185f0781
karnVen <onemonth06@gmail.com>
2026-04-08 06:12:25 +0000

    feat:edit&delete_chips

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -34,4 +34,32 @@ public function store(Request $request)
 
     return redirect('/')->with('success', 'Your chirp has been posted!');
 }
+
+public function edit(Chirp $chirp)
+{
+    // We'll add authorization in lesson 11
+    return view('chirps.edit', compact('chirp'));
+}
+
+public function update(Request $request, Chirp $chirp)
+{
+        $this->authorize('update', $chirp);
+
+    // Validate
+    $validated = $request->validate([
+        'message' => 'required|string|max:255',
+    ]);
+
+    // Update
+    $chirp->update($validated);
+
+    return redirect('/')->with('success', 'Chirp updated!');
+}
+
+public function destroy(Chirp $chirp)
+{
+    $chirp->delete();
+
+    return redirect('/')->with('success', 'Chirp deleted!');
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 // This tells Laravel: Go to ChirpController and run the 'index' function
 Route::get('/', [ChirpController::class, 'index']);
 Route::post('/chirps', [ChirpController::class, 'store']);
+
+Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
+Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
+Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
+
+
+
+//Route::resource('chirps', ChirpController::class)
+   // ->only(['store', 'edit', 'update', 'destroy']);
